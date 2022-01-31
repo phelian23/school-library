e928c3f39f9afcbb92c5a29ce2aeafc1576fee07
phelian23 <femikola23@gmail.com>
2022-01-31 21:02:01 +0400

    Make of_age method private

--- person.rb
+++ person.rb
@@ -3,16 +3,19 @@ class Person
   attr_accessor :name, :age, :parent_permission
 
   def initialize(age, name = 'unknown', parent_permission: true)
+    @id = rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  def of_age?
-    @age >= 18
-  end
-
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
   end
 end
