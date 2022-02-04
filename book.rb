8798ff1901493172bb31d824e3afab330a536411
phelian23 <femikola23@gmail.com>
2022-02-04 09:45:41 +0400

    Add belong-to method

--- book.rb
+++ book.rb
@@ -1,10 +1,10 @@
 class Book
   attr_accessor :title, :author, :rentals
 
-  def initialize(title, author, rentals = [])
+  def initialize(title, author)
     @title = title
     @author = author
-    @rentals = rentals
+    @rentals = []
   end
 
   def add_rental(date, person)
--- person.rb
+++ person.rb
@@ -5,13 +5,13 @@ class Person
   attr_reader :id, :corrector
   attr_accessor :name, :age, :parent_permission, :rentals
 
-  def initialize(age, name = 'unknown', rentals = [], parent_permission: true)
+  def initialize(age, name = 'unknown', parent_permission: true)
     @id = rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
     @corrector = Corrector.new
-    @rentals = rentals
+    @rentals = []
   end
 
   def can_use_services?
